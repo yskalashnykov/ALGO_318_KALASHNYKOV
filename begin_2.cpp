088425f9d13a516e33ebc9a179edad7ff751ff6b
yskalashnykov <y.s.kalashnykov@student.khai.edu>
2026-09-08 11:12:32 +0300

    Add files via upload

---
+++ begin_2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+// begin 2
+int main()
+{
+    // декларація змінних
+    double a, S;
+
+    // введення змінних
+    cout << "введіть сторону квадрата а: ";
+    cin >> a;
+
+    // розрахунок результату
+    S = a * a;
+
+    // результат
+    cout << "площа S: " << S << endl;
+
+    return 0;
+}
